f8207a0b31df50956c86b1821d95031228060c1c
drdilyor <drdilyor@outlook.com>
2021-03-29 20:37:56 +0500

    Add: Procfile

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   CORS(app)
+  @app.route('/')
+  def index():
+      return {'message', 'hello world'}
 
   return app
 
